17ced7f09a0f2bc73af3d326f771c59e8e1badbe
rabinabaga <rabinabaga@gmail.com>
2026-07-24 08:01:26 +0545

    migration edit file

--- pilot_tester/src/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import userRoutes from "../user/routes/user_routes";
-
-const router = Router();
-router.use("/user", userRoutes);
-
-export default router;
--- pilot_tester/src/user/routes/user_routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import UserController from "../controller/user_controller";
-
-const router = Router();
-
-router.post("/signup", UserController.createUser);
-
-export default router;
